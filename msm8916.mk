475c2e34999f83532ea71fdc2a25e18ce1cdf695
Akianonymus <anonymus.aki@gmail.com>
2019-05-13 08:54:01 +0530

    msm8916-common: Build our own powerhal
    * because new lineage one is too new for our old shitty devices
    
    * and this one restore my battery life like nougat days

--- msm8916.mk
+++ msm8916.mk
@@ -229,9 +229,7 @@ PRODUCT_PACKAGES += \
 
 # Power HAL
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service \
-    power.msm8916
+     android.hardware.power@1.0-service.8916
 
 # QMI
 PRODUCT_PACKAGES += \
